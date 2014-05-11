46f2fe92687ecc44bb58eb397c15a8d55f40ad91
Matt Bray <mattjbray@gmail.com>
2014-05-11 23:25:22 +0100

    count inversions: don't use an accumulator

--- week-1/CountInversions.hs
+++ week-1/CountInversions.hs
@@ -12,17 +12,19 @@ countInversions xs = fst $ go (0, xs)
   go :: Ord a => (Int, [a]) -> (Int, [a])
   go (count, []) = (count, [])
   go (count, [x]) = (count, [x])
-  go (count, yzs) = countAndMerge (count, []) (go (0, ys)) (go (0, zs))
+  go (count, yzs) = countAndMerge (go (0, ys)) (go (0, zs))
     where
     (ys, zs) = splitAt (length yzs `div` 2) yzs
 
 countAndMerge :: Ord a
-  => (Int, [a]) -- accumulator
-  -> (Int, [a]) -- input 1
+  => (Int, [a]) -- input 1
   -> (Int, [a]) -- input 2
   -> (Int, [a]) -- result
-countAndMerge (count, res) (xCount, []) (yCount, ys) = (xCount + yCount + count, res ++ ys)
-countAndMerge (count, res) (xCount, xs) (yCount, []) = (xCount + yCount + count, res ++ xs)
-countAndMerge (count, res) (xCount, (x:xs)) (yCount, (y:ys))
-  | (x < y)   = countAndMerge (count, res ++ [x]) (xCount, xs) (yCount, (y:ys))
-  | otherwise = countAndMerge (count + (length (x:xs)), res ++ [y]) (xCount, (x:xs)) (yCount, ys)
+countAndMerge (xCount, []) (yCount, ys) = (xCount + yCount, ys)
+countAndMerge (xCount, xs) (yCount, []) = (xCount + yCount, xs)
+countAndMerge (xCount, (x:xs)) (yCount, (y:ys))
+  | (x < y)   = (count1, x:res1)
+  | otherwise = (count2 + (length (x:xs)), y:res2)
+  where
+  (count1, res1) = countAndMerge (xCount, xs) (yCount, y:ys)
+  (count2, res2) = countAndMerge (xCount, x:xs) (yCount, ys)
