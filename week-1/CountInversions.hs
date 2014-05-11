ec6a635804ee2a7922fb57949f701baf0606a4e5
Matt Bray <mattjbray@gmail.com>
2014-05-12 00:01:13 +0100

    count inversions: convert input to ints

--- week-1/CountInversions.hs
+++ week-1/CountInversions.hs
@@ -1,8 +1,12 @@
+-- Answer: 2407905288
+
 import Control.Monad
+import Test.HUnit
 
 main = do
   input <- (liftM lines . readFile) "IntegerArray.txt"
-  putStrLn $ show $ countInversions input
+  let inputInt = map read input :: [Int]
+  putStrLn $ show $ countInversions inputInt
 
 countInversions :: Ord a
   => [a]
@@ -28,3 +32,12 @@ countAndMerge (xCount, (x:xs)) (yCount, (y:ys))
   where
   (count1, res1) = countAndMerge (xCount, xs) (yCount, y:ys)
   (count2, res2) = countAndMerge (xCount, x:xs) (yCount, ys)
+
+tests = test [ "test1" ~: 0  ~=? (countInversions [1,2,3]),
+               "test2" ~: 1  ~=? (countInversions [1,3,2]),
+               "test3" ~: 3  ~=? (countInversions [1,3,5,2,4,6]),
+               "test4" ~: 4  ~=? (countInversions [1,5,3,2,4]),
+               "test5" ~: 10 ~=? (countInversions [5,4,3,2,1]),
+               "test6" ~: 5  ~=? (countInversions [1,6,3,2,4,5]),
+               "test7" ~: 0  ~=? (countInversions ["1000", "11"]) -- watch out for string inputs!
+             ]
